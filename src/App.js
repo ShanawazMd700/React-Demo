74400a19c1de19c8074f9c599d3f3335d3d03557
MD Shanawaz <shanawaz.md@i-raysolutions.com>
2025-11-18 12:35:47 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="logincontainer">
+      <div className="loginbox">
+        <div className="loginleft">
+          <h2>Login</h2>
+          <p className="subtitle">Sign in to your account</p>
+          <div className="inputgroup">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button className="loginbtn">Login</button>
+          </div>
+          <p className="forgot">Forgot password?</p>
+        </div>
+        <div className="loginright">
+          <h1>Password Generator</h1>
+          <h2>Version1.0</h2>
+          <p>
+            This page will provide new passwords and other solutions for your accounts which
+            makes your web surfing safe and secure
+          </p>
+        </div>
+
+      </div>
     </div>
   );
 }
