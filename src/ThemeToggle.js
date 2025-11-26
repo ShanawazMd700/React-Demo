776b4baf6f8af8cb52e537ea4116bba707139e82
MD Shanawaz <shanawaz.md@i-raysolutions.com>
2025-11-26 16:57:58 +0530

    Sixth Commit

--- src/ThemeToggle.js
+++ src/ThemeToggle.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
+
 import { useTheme } from "./ThemeContext";
 
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button onClick={toggleTheme}>
-      {theme === "light" ? "🌙 Dark" : "☀ Light"}
+        <button className="theme-toggle-btn" onClick={toggleTheme}>
+      {theme === "light" ? (
+        <FaMoon size={22} />
+      ) : (
+        <FaSun size={22} />
+      )}
     </button>
   );
 }
